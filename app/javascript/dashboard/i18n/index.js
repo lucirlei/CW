de3db8269540c353b5c6fd74e3b2a3bfe2467c07
lucirlei <156731932+lucirlei@users.noreply.github.com>
2024-07-13 10:31:06 -0300

    MixCW_Uno
    new

---
+++ app/javascript/dashboard/i18n/index.js
@@ -0,0 +1,9 @@
+import en from './locale/en';
+import es from './locale/es';
+import pt_BR from './locale/pt_BR';
+
+export default {
+  en,
+  es,
+  pt_BR,
+};
---
+++ app/javascript/survey/i18n/index.js
@@ -0,0 +1,9 @@
+import en from './locale/en.json';
+import es from './locale/es.json';
+import pt_BR from './locale/pt_BR.json';
+
+export default {
+  en,
+  es,
+  pt_BR,
+};
